260ebc07273a9eb0b1d53ac0c0d31b41b4283fb1
yeffreycode <yeffreyespinoza200@gmail.com>
2022-09-05 15:21:04 -0500

    completed parsing request paths

--- app/index.js
+++ app/index.js
@@ -9,7 +9,18 @@ var url = require("url");
 
 //thes erver should respond to all requests with a string
 var server = http.createServer(function (req, res) {
+  //get the url and parse it
+  var parseUrl = url.parse(req.url, true);
+
+  // get the path
+  var path = parseUrl.pathname;
+  var trimePath = path.replace(/^\/+|\/+$/g, "");
+
+  //send the response
   res.end("hello world\n");
+
+  //log the request path
+  console.log("Request received on path: " + trimePath);
 });
 
 //start the server, nad have it lsten on port 3000

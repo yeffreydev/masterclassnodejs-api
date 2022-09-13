c7cb1efbfad8611f9a178da7b42597455559f641
yeffreycode <yeffreyespinoza200@gmail.com>
2022-09-13 10:18:54 -0500

    complete adding https support

--- index.js
+++ index.js
@@ -5,12 +5,38 @@
 //dependencies
 
 var http = require("http");
+var https = require("https");
 var url = require("url");
 var StringDecoder = require("string_decoder").StringDecoder;
 var config = require("./config");
+var fs = require("fs");
 
-//thes erver should respond to all requests with a string
-var server = http.createServer(function (req, res) {
+//Instantiate the HTTP server
+var httpServer = http.createServer(function (req, res) {
+  unifiedServer(req, res);
+});
+
+//start the HTTP server
+httpServer.listen(config.httpPort, function () {
+  console.log("the http server is listening on port " + config.httpPort);
+});
+
+//Instantiate the HTTPS server
+var httpsServerOptions = {
+  key: fs.readFileSync("./https/key.pem"),
+  cert: fs.readFileSync("./https/cert.pem"),
+};
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+  unifiedServer(req, res);
+});
+
+//Start the HTTPS server
+httpsServer.listen(config.httpsPort, function () {
+  console.log("the  https server is listening on port " + config.httpsPort);
+});
+
+// All the server logic for both the http and https server
+var unifiedServer = function (req, res) {
   //get the url and parse it
   var parseUrl = url.parse(req.url, true);
 
@@ -71,18 +97,7 @@ var server = http.createServer(function (req, res) {
       console.log("Return this response: ", statusCode, payloadString);
     });
   });
-});
-
-//start the server
-server.listen(config.port, function () {
-  console.log(
-    "the server is listening on port " +
-      config.port +
-      " in " +
-      config.envName +
-      " mode"
-  );
-});
+};
 
 //Define the handleres
 var handlers = {};

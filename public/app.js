8851a9f214ca19b8bd62f1e09649ce7998c279b0
yeffreycode <yeffreyespinoza200@gmail.com>
2022-12-15 16:57:05 -0500

    completed edit account task

--- public/app.js
+++ public/app.js
@@ -329,6 +329,59 @@ app.renewToken = function (callback) {
   }
 };
 
+// Load data on the page
+app.loadDataOnPage = function () {
+  // Get the current page from the body class
+  var bodyClasses = document.querySelector("body").classList;
+  var primaryClass = typeof bodyClasses[0] == "string" ? bodyClasses[0] : false;
+
+  // Logic for account settings page
+  if (primaryClass == "accountEdit") {
+    app.loadAccountEditPage();
+  }
+
+  // Logic for dashboard page
+  if (primaryClass == "checksList") {
+    app.loadChecksListPage();
+  }
+
+  // Logic for check details page
+  if (primaryClass == "checksEdit") {
+    app.loadChecksEditPage();
+  }
+};
+
+// Load the account edit page specifically
+app.loadAccountEditPage = function () {
+  // Get the phone number from the current token, or log the user out if none is there
+  var phone = typeof app.config.sessionToken.phone == "string" ? app.config.sessionToken.phone : false;
+  if (phone) {
+    // Fetch the user data
+    var queryStringObject = {
+      phone: phone,
+    };
+    app.client.request(undefined, "api/users", "GET", queryStringObject, undefined, function (statusCode, responsePayload) {
+      if (statusCode == 200) {
+        // Put the data into the forms as values where needed
+        document.querySelector("#accountEdit1 .firstNameInput").value = responsePayload.firstName;
+        document.querySelector("#accountEdit1 .lastNameInput").value = responsePayload.lastName;
+        document.querySelector("#accountEdit1 .displayPhoneInput").value = responsePayload.phone;
+
+        // Put the hidden phone field into both forms
+        var hiddenPhoneInputs = document.querySelectorAll("input.hiddenPhoneNumberInput");
+        for (var i = 0; i < hiddenPhoneInputs.length; i++) {
+          hiddenPhoneInputs[i].value = responsePayload.phone;
+        }
+      } else {
+        // If the request comes back as something other than 200, log the user our (on the assumption that the api is temporarily down or the users token is bad)
+        app.logUserOut();
+      }
+    });
+  } else {
+    app.logUserOut();
+  }
+};
+
 // Loop to renew token often
 app.tokenRenewalLoop = function () {
   setInterval(function () {
@@ -353,6 +406,9 @@ app.init = function () {
 
   // Renew token
   app.tokenRenewalLoop();
+
+  //Load data on page
+  app.loadDataOnPage();
 };
 
 // Call the init processes after the window loads

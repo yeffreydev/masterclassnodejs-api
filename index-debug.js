38751e3499bbba0875394c68bc5cb31f5b7f3779
yeffreycode <yeffreyespinoza200@gmail.com>
2022-12-18 18:41:13 -0500

    completed using the debugger task

---
+++ index-debug.js
@@ -0,0 +1,63 @@
+/**
+ * primary file for the api
+ */
+
+//Dependencies
+var server = require("./lib/server");
+var workers = require("./lib/wokers");
+var cli = require("./lib/cli");
+var exampleDebugginProblem = require("./lib/exampleDebugginProblem");
+
+// Declare the app
+var app = {};
+//Init function
+app.init = function () {
+  //Start the server
+  debugger;
+  server.init();
+  debugger;
+  //Start the workers
+  debugger;
+  workers.init();
+  debugger;
+
+  //Start the Cli, but make sure it starts last
+  debugger;
+  setTimeout(function () {
+    cli.init();
+  }, 50);
+  debugger;
+
+  //Set foo at 1
+  debugger;
+  var foo = 1;
+  console.log("Just assigned 1 to foo");
+  debugger;
+
+  //Increment foo
+  foo++;
+  console.log("Just incremented foo");
+  debugger;
+
+  //Square foo
+  foo = foo * foo;
+  console.log("Just Squared foo");
+  debugger;
+
+  //Convert foo to a string
+  foo = foo.toString();
+  console.log("Just converted foo to string");
+  debugger;
+
+  //call the init script that will throw
+  exampleDebugginProblem.init();
+  console.log("Just called the library");
+  debugger;
+};
+
+//execute
+app.init();
+
+//export the app;
+
+module.exports = app;
